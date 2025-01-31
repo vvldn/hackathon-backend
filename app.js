655adf7bf124b712a7db9266bd0a5924ac5ac1ea
Vighnesh V <vighnesh.v@myhq.in>
2025-02-01 02:16:18 +0530

    PUT CORS FIX

--- app.js
+++ app.js
@@ -31,7 +31,7 @@ var corsOptions = {
 app.use(function(req,res,next){
     res.setHeader( "Access-Control-Allow-Origin", req.headers.origin || '*');
     res.setHeader( "Access-Control-Allow-Credentials", 'true');
-    res.setHeader( "Access-Control-Allow-Methods", "GET,POST,DELETE");
+    res.setHeader( "Access-Control-Allow-Methods", "GET,POST,DELETE,PUT");
     res.setHeader('Access-Control-Allow-Headers','*');
     next();
 });
