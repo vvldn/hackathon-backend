b9386242d3b0717da8f0f6662db37cc0ea49b67c
Vighnesh V <vighnesh.v@myhq.in>
2025-01-31 22:34:06 +0530

    custom error handler

--- app.js
+++ app.js
@@ -58,14 +58,9 @@ app.use(function(req, res, next) {
   next(err);
 });
 
-// error handler
-app.use(function(err, req, res, next) {
-  // set locals, only providing error in development
-  res.locals.message = err.message;
-  res.locals.error = req.app.get('env') === 'development' ? err : {};
-
-  // render the error page
-  res.status(err.status || 500);
-});
+function errorHandlerCustom(err, req, res, next) {
+  console.log(`Error: ${err.message}, Error Handler Stack: ${err.stack}`);
+return res.status(httpErrorStatusCodes.INTERNAL_SERVER_ERROR).send({ reason: `Internal Server Error. Please try again later. ${err.message} - ${err.stack} ` });
+}
 
 module.exports = app;
