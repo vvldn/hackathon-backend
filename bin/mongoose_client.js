dda6221bd0b648b1b23ab6e65dd888e33fc59b85
Vighnesh V <vighnesh.v@myhq.in>
2025-01-31 21:14:54 +0530

    new db uri

--- bin/mongoose_client.js
+++ bin/mongoose_client.js
@@ -5,6 +5,7 @@ var connectDB = function (callback, fallback) {
     mongoose.connect(properties.db_properties.prod.url);
     var db = mongoose.connection;
     db.on('error',function (error) {
+        console.log('db connection errored', JSON.stringify(error));
         fallback(error);
     });
     db.once('open',function (obj) {
